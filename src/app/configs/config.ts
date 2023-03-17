4f50ff3283625d8265f36fbdcf570cc750838ced
Arvin Han <arvinhjt0718@gmail.com>
2023-03-17 17:55:01 +1100

    QA-1075: update `advtemplate, fontsizeinput, advcode:inline`

--- src/app/configs/config.ts
+++ src/app/configs/config.ts
@@ -48,8 +48,75 @@ const mergeTagsLits = [
   }
 ];
 
+const advtemplate_templates = [
+  {
+    id: '1',
+    title: 'Quick replies',
+    items: [
+      {
+        id: '2',
+        title: 'Message received',
+        content: '<p dir="ltr">Hey {{Customer.FirstName}}!</p>\n<p dir="ltr">Just a quick note to say we&rsquo;ve received your message, and will get back to you within 48 hours.</p>\n<p dir="ltr">For reference, your ticket number is: {{Ticket.Number}}</p>\n<p dir="ltr">Should you have any questions in the meantime, just reply to this email and it will be attached to this ticket.</p>\n<p><strong>&nbsp;</strong></p>\n<p dir="ltr">Regards,</p>\n<p dir="ltr">{{Agent.FirstName}}</p>'
+      },
+      {
+        id: '3',
+        title: 'Thanks for the feedback',
+        content: '<p dir="ltr">Hi {{Customer.FirstName}},</p>\n<p dir="ltr">We appreciate you taking the time to provide feedback on {{Product.Name}}.</p>\n<p dir="ltr">It sounds like it wasn&rsquo;t able to fully meet your expectations, for which we apologize. Rest assured our team looks at each piece of feedback and uses it to decide what to focus on next with {{Product.Name}}.</p>\n<p dir="ltr"><strong>&nbsp;</strong></p>\n<p dir="ltr">All the best, and let us know if there&rsquo;s anything else we can do to help.</p>\n<p dir="ltr">-{{Agent.FirstName}}</p>'
+      },
+      {
+        id: '6',
+        title: 'Still working on case',
+        content: '<p dir="ltr"><img src="https://lh4.googleusercontent.com/-H7w_COxrsy2fVpjO6RRnoBsujhaLyg6AXux5zidqmQ_ik1mrE6BtnaTUdWYQuVbtKpviRqQiuPBOHNGUsEXvrRliEHc4-hKDrCLgQQ9Co-MI4uY2ehUvYtU1nn3EeS0WiUzST-7MQB2Z5YFXrMDwRk" width="320" height="240"></p>\n<p><strong>&nbsp;</strong></p>\n<p dir="ltr">Hi {{Customer.FirstName}},</p>\n<p dir="ltr">Just a quick note to let you know we&rsquo;re still working on your case. It&rsquo;s taking a bit longer than we hoped, but we&rsquo;re aiming to get you an answer in the next 48 hours.</p>\n<p dir="ltr">Stay tuned,</p>\n<p dir="ltr">{{Agent.FirstName}}</p>'
+      }
+    ]
+  },
+  {
+    id: '4',
+    title: 'Closing tickets',
+    items: [
+      {
+        id: '7',
+        title: 'Closing ticket',
+        content: '<p dir="ltr">Hi {{Customer.FirstName}},</p>\n<p dir="ltr">We haven&rsquo;t heard back from you in over a week, so we have gone ahead and closed your ticket number {{Ticket.Number}}.</p>\n<p dir="ltr">If you&rsquo;re still running into issues, not to worry, just reply to this email and we will re-open your ticket.</p>\n<p><strong>&nbsp;</strong></p>\n<p dir="ltr">All the best,</p>\n<p dir="ltr">{{Agent.FirstName}}</p>'
+      },
+      {
+        id: '8',
+        title: 'Post-call survey',
+        content: '<p dir="ltr">Hey {{Customer.FirstName}}!</p>\n<p dir="ltr">&nbsp;</p>\n<p dir="ltr">How did we do?</p>\n<p dir="ltr">If you have a few moments, we&rsquo;d love you to fill out our post-support survey: {{Survey.Link}}</p>\n<p><strong>&nbsp;</strong></p>\n<p dir="ltr">Thanks in advance!<br>{{Company.Name}} Customer Support</p>'
+      }
+    ]
+  },
+  {
+    id: '5',
+    title: 'Product support',
+    items: [
+      {
+        id: '9',
+        title: 'How to find model number',
+        content: '<p dir="ltr">Hi {{Customer.FirstName}},</p>\n<p><strong>&nbsp;</strong></p>\n<p dir="ltr">My name is {{Agent.FirstName}} and I will be glad to assist you today.</p>\n<p dir="ltr">To troubleshoot your issue, we first need your model number, which can be found on the underside of your product beneath the safety warning label.&nbsp;</p>\n<p dir="ltr">It should look something like the following: XX.XXXXX.X</p>\n<p dir="ltr">Once you send it over, I will advise on next steps.</p>\n<p><strong>&nbsp;</strong></p>\n<p dir="ltr">Thanks!</p>\n<p dir="ltr">{{Agent.FirstName}}</p>'
+      },
+      {
+        id: '10',
+        title: 'Support escalation',
+        content: '<p dir="ltr"><img src="https://lh3.googleusercontent.com/z4hleIymnERrS9OQQMBwmkqVne8kYZA0Kly9Ny64pp4fi47CWWUy30Q0-UkjGf-K-50zrfR-wltHUTbExzZ7VUSUAUG60Fll5f2E0UZcKjKoa-ZVlIcuOoe-RRckFWqiihUOfVds7pXtM8Y59uy2hpw" width="295" height="295"></p>\n<p><strong>&nbsp;</strong></p>\n<p dir="ltr">Hi {{Customer.FirstName}},</p>\n<p dir="ltr">We have escalated your ticket {{Ticket.Number}} to second-level support.</p>\n<p dir="ltr">You should hear back from the new agent on your case, {{NewAgent.FirstName}}, shortly.</p>\n<p><strong>&nbsp;</strong></p>\n<p dir="ltr">Thanks,</p>\n<p dir="ltr">{{Company.Name}} Customer Support</p>'
+      }
+    ]
+  },
+  {
+    id: '6',
+    title: 'Email Signatures',
+    items: [
+      {
+        id: '11',
+        title: 'Tiny Signature',
+        content: '<p>Regards<br /><br /><br /><a title="Tiny Technologies Inc" href="https://www.tiny.cloud/" target="_blank"><img src="https://www.tiny.cloud/email-static/tiny-logo.png" alt="Tiny Technologies Inc" width="130" height="42" /></a></p><p>Shiridi Gandham<br />QA Template Manager</p><p>Email:&nbsp;<a title="Email Shiridi" href="mailto:shiridi.gandham@tiny.cloud" target="_blank">shiridi.gandham@tiny.cloud</a><br />Phone:&nbsp;(+617) 3161 3557</p><p>Tiny Technologies<br /><a title="Tiny Technologies" href="https://www.tiny.cloud/" target="_blank">www.tiny.cloud</a></p><p><a title="Linkedin" href="https://www.linkedin.com/company/jointiny/" target="_blank" aria-invalid="true"><img src="https://www.tiny.cloud/email-static/social-linkedin.png" alt="Linkedin" width="20" height="20" /></a>&nbsp;<a title="Facebook" href="https://www.facebook.com/jointiny" target="_blank"><img src="https://www.tiny.cloud/email-static/social-facebook.png" alt="Facebook" width="20" height="20" /></a>&nbsp;<a title="Twitter" href="https://twitter.com/joinTiny" target="_blank"><img src="https://www.tiny.cloud/email-static/social-twitter.png" alt="Twitter" width="20" height="20" /></a>&nbsp;<a title="GitHub" href="https://github.com/tinymce/" target="_blank"><img src="https://www.tiny.cloud/email-static/social-github.png" alt="GitHub" width="20" height="20" /></a>&nbsp;<a title="Stack Overflow" href="https://stackoverflow.com/questions/tagged/tinymce" target="_blank"><img src="https://www.tiny.cloud/email-static/social-stackoverflow.png" alt="Stack Overflow" width="20" height="20" /></a></p>'
+      }
+    ]
+  }
+];
+
 export const c1 = `{
-  plugins: "advlist autolink autocorrect mergetags footnotes lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table help wordcount typography inlinecss",
+  plugins: "advlist advtemplate autolink autocorrect mergetags footnotes lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table help wordcount typography inlinecss",
   height: 600,
   mergetags_prefix: '\${',
   mergetags_suffix: '}',
@@ -57,7 +124,9 @@ export const c1 = `{
   autocorrect_autocorrect: true,
   autocorrect_capitalize: true,
   toolbar:
-    "insertfile undo redo | styles typography | bold italic | wordcount | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | footnotes footnotesupdate | table help",
+    "insertfile undo redo addtemplate inserttemplate | styles typography fontsizeinput| bold italic | wordcount | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | footnotes footnotesupdate | table help",
+  advcode_inline: true,
+  advtemplate_templates: ${JSON.stringify(advtemplate_templates)},
   mobile: {
     theme: "silver",
     plugins: "casechange link image lists advlist anchor code codesample preview table textpattern help wordcount",
@@ -74,9 +143,11 @@ export const c2 = `{
   mergetags_list: ${JSON.stringify(mergeTagsLits)},
   autocorrect_autocorrect: true,
   autocorrect_capitalize: true,
-  plugins: "advlist autolink autocorrect mergetags footnotes lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table help typography inlinecss",
+  plugins: "advlist advtemplate autolink autocorrect mergetags footnotes lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table help typography inlinecss",
   toolbar:
-    "insertfile undo redo | styles typography | bold italic | permanentpen | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | footnotes footnotesupdate | table help",
+    "insertfile undo redo addtemplate inserttemplate | styles typography fontsizeinput | bold italic | permanentpen | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | footnotes footnotesupdate | table help",
+  advcode_inline: true,
+  advtemplate_templates: ${JSON.stringify(advtemplate_templates)},
   mobile: {
     theme: "silver",
     plugins: "casechange lists advlist anchor code codesample preview table textpattern help",
@@ -86,18 +157,20 @@ export const c2 = `{
 }`;
 
 export const c3 = `{
-  plugins: "quickbars advlist autocorrect mergetags footnotes autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table help typography inlinecss",
+  plugins: "quickbars advlist advtemplate autocorrect mergetags footnotes autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table help typography inlinecss",
   mergetags_prefix: '\${',
   mergetags_suffix: '}',
   mergetags_list: ${JSON.stringify(mergeTagsLits)},
   autocorrect_autocorrect: true,
   autocorrect_capitalize: true,
   toolbar:
-    "insertfile undo redo | styles typography | bold italic | permanentpen | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | footnotes footnotesupdate | table help",
+    "insertfile undo redo addtemplate inserttemplate | styles typography fontsizeinput | bold italic | permanentpen | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | footnotes footnotesupdate | table help",
   quickbars_insert_toolbar:
     "quicktable quickimage quicklink styles ",
   quickbars_selection_toolbar:
     "bold italic | h2 h3 | blockquote | code| forecolor backcolor | quicktable quickimage | styles ",
+  advcode_inline: true,
+  advtemplate_templates: ${JSON.stringify(advtemplate_templates)},
   setup: function (editor) {
     editor.ui.registry.addContextToolbar("imagealignment", {
       predicate: function (node) {
@@ -132,23 +205,25 @@ export const c3 = `{
 }`;
 
 export const c4 = `{
-  plugins: "quickbars advlist autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table help typography inlinecss",
+  plugins: "quickbars advlist advtemplate autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table help typography inlinecss",
   toolbar_mode: "sliding",
   toolbar_sticky: true,
   toolbar_location: "bottom",
   toolbar:
-      "insertfile undo redo | styles typography | bold italic | permanentpen | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | table help",
+      "insertfile undo redo addtemplate inserttemplate | styles typography fontsizeinput | bold italic | permanentpen | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | table help",
   quickbars_insert_toolbar:
       "quicktable quickimage quicklink styles ",
   quickbars_selection_toolbar:
       "bold italic | h2 h3 | blockquote | code| forecolor backcolor | quicktable quickimage | styles ",
+  advcode_inline: true,
+  advtemplate_templates: ${JSON.stringify(advtemplate_templates)},
 }`;
 
 export const c5 = `{
   plugins: [
     "advtable", "autoresize", "anchor", "advlist", "autolink", "autosave", "charmap", "advcode", "codesample", "directionality", "emoticons", "fullscreen",
     "help", "image", "insertdatetime", "importcss", "link", "lists", "media", "nonbreaking", "pagebreak", "preview", "save", "searchreplace", "table",
-    "template", "visualblocks", "visualchars", "wordcount", "casechange", "checklist", "powerpaste", "a11ychecker", "tinymcespellchecker", "tinydrive",
+    "advtemplate", "visualblocks", "visualchars", "wordcount", "casechange", "checklist", "powerpaste", "a11ychecker", "tinymcespellchecker", "tinydrive",
     "tableofcontents", "editimage", "mentions", "mediaembed", "permanentpen", "formatpainter", "pageembed", "linkchecker", "tinycomments", "export",
     "autocorrect", "mergetags", "footnotes", "typography", "inlinecss"
   ],
@@ -169,10 +244,12 @@ export const c5 = `{
   tinycomments_author_name: 'SuperCoolQA',
   quickbars_insert_toolbar: 'quicktable quickimage quicklink styles',
   quickbars_selection_toolbar: 'bold italic | h2 h3 | blockquote | code| forecolor backcolor typography| quicktable quickimage | formatpainter pageembed permanentpen styles ',
-  toolbar: "bold italic underline strikethrough subscript superscript | fontfamily fontsize | numlist bullist checklist | permanentpen formatpainter removeformat forecolor backcolor | blockquote nonbreaking hr pagebreak | casechange styles blocks lineheight | ltr rtl outdent indent | align alignleft aligncenter alignright alignjustify alignnone | h1 h2 h3 h4 h5 h6 h7 |" +
+  toolbar: "bold italic underline strikethrough subscript superscript addtemplate inserttemplate | fontfamily fontsize fontsizeinput | numlist bullist checklist | permanentpen formatpainter removeformat forecolor backcolor | blockquote nonbreaking hr pagebreak | casechange styles blocks lineheight | ltr rtl outdent indent | align alignleft aligncenter alignright alignjustify alignnone | h1 h2 h3 h4 h5 h6 h7 |" +
     "copy cut paste pastetext selectall remove newdocument wordcount searchreplace | undo redo | save cancel restoredraft | fullscreen print preview export code help | template insertdatetime codesample emoticons charmap | anchor link unlink image media pageembed insertfile | visualblocks visualchars a11ycheck | spellchecker language spellcheckdialog | tableofcontents tableofcontentsupdate | " +
     "table advtablerownumbering tableclass tablecellclass tablecellvalign tablecellborderwidth tablecellborderstyle tablecaption tablecellbackgroundcolor tablecellbordercolor tablerowheader tablecolheader",
   menubar: 'file edit insert view format table footnotes footnotesupdate | tools help',
+  advcode_inline: true,
+  advtemplate_templates: ${JSON.stringify(advtemplate_templates)},
   mobile: {
     theme: "silver",
     plugins: [
